05e62e87abb327cc00fe94bff0184cd96198e1ef
bsfonseca <bs.fonseca12@gmail.com>
2023-08-28 11:07:56 -0300

    Versão Final

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
 const lista = document.getElementById("lista");
+let page = 1;
 
 function atualizarLista(personagens) {
+  lista.innerHTML = "";
   for (let item of personagens) {
-    const li = document.createElement("div");
-    li.innerHTML = `Personagem ${item.id}: ${item.name} - ${item.species}`;
+    const divPersonagem = document.createElement("div");
+    divPersonagem.classList.add("personagem-card");
 
-    const imagem = document.createElement("img");
-    imagem.setAttribute("height", "100");
-    imagem.setAttribute("src", item.image);
-    imagem.classList.add("img-personagem");
+    const imagem = document.createElement("div");
+    imagem.classList.add("personagem-imagem");
 
-    li.appendChild(imagem);
-    lista.appendChild(li);
+    const img = document.createElement("img");
+    img.setAttribute("src", item.image);
+    imagem.appendChild(img);
+
+    const info = document.createElement("div");
+    info.classList.add("personagem-info");
+
+    const nome = document.createElement("h2");
+    nome.textContent = item.name;
+
+    const especie = document.createElement("p");
+    especie.textContent = `Espécie: ${item.species}`;
+
+    const status = document.createElement("p");
+    status.textContent = `Status: ${item.status}`;
+
+    info.appendChild(nome);
+    info.appendChild(especie);
+    info.appendChild(status);
+
+    divPersonagem.appendChild(imagem);
+    divPersonagem.appendChild(info);
+
+    lista.appendChild(divPersonagem);
   }
 }
 
-function listarPersonagens() {
+function listarPersonagens(page) {
   axios
-    .get("https://rickandmortyapi.com/api/character")
+    .get(`https://rickandmortyapi.com/api/character/?page=${page}`)
     .then((result) => {
       atualizarLista(result.data.results);
     })
@@ -26,4 +48,34 @@ function listarPersonagens() {
     });
 }
 
-listarPersonagens();
+document.getElementById("bt-proximo").addEventListener("click", () => {
+  page++;
+  listarPersonagens(page);
+});
+
+document.getElementById("bt-anterior").addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    listarPersonagens(page);
+  }
+});
+
+listarPersonagens(page);
+
+document.getElementById("btn-busca").addEventListener("click", () => {
+  const nomePersonagem = document.getElementById("campo-busca").value;
+  if (nomePersonagem.trim() !== "") {
+    buscarPersonagem(nomePersonagem);
+  }
+});
+
+function buscarPersonagem(nome) {
+  axios
+    .get(`https://rickandmortyapi.com/api/character/?name=${nome}`)
+    .then((result) => {
+      atualizarLista(result.data.results);
+    })
+    .catch((erro) => {
+      console.log(erro);
+    });
+}
